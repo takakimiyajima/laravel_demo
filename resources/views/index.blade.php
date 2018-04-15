7cf52ca5500b64fcf549172a6e8b6e4b4997f6c2
takakimiyajima <>
2018-04-15 15:57:03 +0900

    index.bladeの設定

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,18 @@
-<!doctype html>
-<html>
-<head>
-	<title>テスト</title>
-</head>
-<body>
-	<h1>bladeテスト</h1>
+@extends('layout.helloapp')
 
-	@section('content')
-		<p>ここが本文のコンテンツです。</p>
-		<p>'message' = {{ $message }}</p>
-		<p>'view_message' = {{ $view_message }}</p>
-	@endsection
-</body>
-</html>
+@section('title', 'ここにタイトル')
+
+@section('menubar')
+	@parent
+	インデックスページ
+@endsection
+
+@section('content')
+	<p>ここが本文のコンテンツです</p>
+	<p>これは、<middleware>google.com</middleware>へのリンクです</p>
+	<p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです</p>
+@endsection
+
+@section('footer')
+copyright miyajima
+@endsection
